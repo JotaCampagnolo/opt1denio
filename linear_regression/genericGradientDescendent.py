f932028c147619d519a0003dbabb20a7eb33a4cf
João M. Campagnolo <jota.campagnolo@gmail.com>
2018-03-28 11:41:42 -0300

    Linear Regression Almost 100%.

--- linear_regression/genericGradientDescendent.py
+++ linear_regression/genericGradientDescendent.py
@@ -24,24 +24,78 @@ print("[3] : Data Dimensions:")
 print("      *", m, "samples.")
 print("      *", d, "features.\n")
 
+# Calculating Train Samples Size:
+TRAIN_SAMPLES = int(TRAIN_SIZE*m)
+print("[4] : Definition of Train Samples Size:")
+print("      *", TRAIN_SAMPLES, "samples\n")
+
 # Instanciation of DATA matrices:
 X = np.array([line.split(',')[0:d] for line in inputDATA], dtype = float)
 y = np.array([line.split(',')[d:(d+1)] for line in inputDATA], dtype = float)
-print("[4] : Data Matrices:")
+print("[5] : Data Matrices:")
 print("      * X shape:", X.shape, "rows/cols")
 print("      * y shape:", y.shape, "rows/cols\n")
 
 # Instanciation of Theta Matrix:
-Theta = np.array([[-175.0, 1.0]])
-print("[5] : Theta Matrix Values:")
+Theta = np.array([[0.0, 0.0]])
+print("[6] : Theta Matrix Values:")
 for i in range(0, len(Theta[0])):
     print("      * Theta", i, "inicialized as", Theta[0][i])
 print()
 
 # Discretizing DATA features:
 DX = X / (np.max(X, 0))
-print("[6] : The X matrix was discretized with:")
-print("      * MAX value:", np.max(X))
-print("      * MIN value:", np.min(X))
+print("[7] : The X matrix was discretized with:")
+print("      * MAX value:", np.max(DX))
+print("      * MIN value:", np.min(DX))
+print()
+
+# Inserting the 1 column in X matrix:
+DX = np.insert(DX,0,1,axis=1)
+print("[8] : Data Matrices after Theta 0 column:")
+print("      * X shape:", DX.shape, "rows/cols")
+print("      * y shape:", y.shape, "rows/cols\n")
 
 # Separating Train and Test Data:
+Xtrain = DX[:TRAIN_SAMPLES]
+Xtest = DX[TRAIN_SAMPLES:]
+ytrain = y[:TRAIN_SAMPLES]
+ytest = y[TRAIN_SAMPLES:]
+print("[9] : Train and Test Data Matrices:")
+print("      * Xtrain shape:", Xtrain.shape, "rows/cols")
+print("      * Xtest shape:", Xtest.shape, "rows/cols")
+print("      * ytrain shape:", ytrain.shape, "rows/cols")
+print("      * ytest shape:", ytest.shape, "rows/cols\n")
+
+# Defining the Cost Function:
+def costFunction(X, y, Theta):
+	m = X.shape[0] # Amount of Samples in this Dataset.
+	y_hat = X.dot(Theta.T) # Once Theta is a list, you have to translate the Theta matrix.
+	cost = np.sum((y_hat - y)**2)
+	return cost/(2*m)
+
+# Defining the Gradient Descendent function:
+def gradientDescendent(X, y, Theta, alpha, iters):
+    print("[G] : Starting Gradient Descendent Function:")
+    J = [] # List of all calculated costs
+    m = X.shape[0] # Amount of Samples in this Dataset
+    for i in range(1,iters):
+        y_hat = X.dot(Theta.T)
+        error = (y_hat - y)
+        error = error * X
+        error = np.sum(error,0)/m
+        Theta = Theta -(alpha*error)
+        J.append(costFunction(X, y, Theta))
+        print("      * Iteracao", i, "-> Theta:", Theta, "and Cost:", J[-1])
+    return Theta, J[1:]
+
+# MAIN:
+finalTheta, finalCost = gradientDescendent(Xtrain, ytrain, Theta, 0.01, 10000)
+y_hat = Xtest.dot(finalTheta.T)
+
+pl.title("Gradient Descendent")
+pl.ylabel("Profit")
+pl.xlabel("Feature")
+pl.plot(Xtest[:,1], ytest, '*')
+pl.plot(Xtest[:,1], y_hat)
+pl.show()
