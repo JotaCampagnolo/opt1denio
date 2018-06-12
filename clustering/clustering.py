35cba5b2e61f0f8b2764e5fa235d88f175c6284e
aluno-uffs <Aluno UFFS>
2018-06-12 09:35:05 -0300

    Clustering seems to be working fine.

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -8,7 +8,7 @@ print("[0] : The CLUSTERING using K-MEANS has started!\n")
 # Program Definitions:
 NUM_CLUST = 2 # Amount of Clusters.
 print("[1] : Program Definitions:")
-print("      * Nothing Yet\n")
+print("      * Number of Clusters:", NUM_CLUST, "\n")
 
 # Openning INPUT FILE:
 inputFILE = open('admission.txt')
@@ -17,15 +17,17 @@ print("[2] : Input FILE was successfully oppened!\n")
 
 # Collecting INPUT DATA dimensions:
 m = len(inputDATA) # Samples Amount.
-d = (np.array([line.split(',')[:] for line in inputDATA]).shape[1]) # Features Amount.
+d = (np.array([line.split(',')[:] for line in inputDATA]).shape[1] - 1) # Features Amount.
 print("[3] : Data Dimensions:")
 print("      *", m, "samples.")
 print("      *", d, "features.\n")
 
 # Instanciation of DATA matrices:
-X = np.array([line.split(',')[0:d+1] for line in inputDATA], dtype = float)
+X = np.array([line.split(',')[0:d] for line in inputDATA], dtype = float)
+y = np.array([line.split(',')[d:(d+1)] for line in inputDATA], dtype = np.int64)
 print("[4] : Data Matrices:")
-print("      * X shape:", X.shape, "rows/cols\n")
+print("      * X shape:", X.shape, "rows/cols")
+print("      * y shape:", y.shape, "rows/cols\n")
 
 # Adding and Removing Features:
 # X = np.delete(X, [0, 3, 9], axis=1)
@@ -37,10 +39,32 @@ print("[5] : Data New Dimensions (changes in cases we remove some feature):")
 print("      *", m, "samples.")
 print("      *", d, "features.\n")
 
+# Ploting the original DATA:
+pl.scatter(X[:,0], X[:,1], c=y[:,0])
+pl.show()
+
 # Defining the Clusters Centroids:
 C = np.zeros((NUM_CLUST,d))
-print(C)
 for i in range(NUM_CLUST):
+    print(i)
     rdm = np.random.randint(m)
     C[i] = X[rdm]
 print(C)
+
+# Defining the Centroids Association Vector:
+centr = np.zeros((m,1))
+print(centr)
+
+# Clustering Routine:
+for j in range(10):
+    for i in range(m):
+        di = np.sum(np.sqrt((X[i,:] - C)**2), 1)
+        centr[i] = np.argmin(di)
+    for i in range(NUM_CLUST):
+        print(i)
+        all_points = X[centr[:,0] == i]
+        mean_points = np.mean(all_points, axis=0)
+        C[i] = mean_points
+    pl.scatter(X[:,0], X[:,1], c=centr[:,0])
+    pl.plot(C[:,0], C[:,1], 'x', c='k')
+    pl.show()
